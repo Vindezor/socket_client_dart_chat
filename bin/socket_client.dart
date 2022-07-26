52408eb0fddf2c5a4016a51a5175841062a4c95c
richard <richarddavidrv13@gmail.com>
2022-07-26 09:58:58 -0400

    created client

---
+++ bin/socket_client.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:socket_io_client/socket_io_client.dart';
+
+void main(List<String> args) {
+  final client = io(
+    'http://172.16.90.127:3000/chat',
+    <String, dynamic> {'transports': ['websocket']}
+  );
+
+  client.onConnect((data) {
+    print("Conectado");
+    client.emit("name", args[0]);
+    readLine().listen((String event) {
+      client.emit('msg' ,event);
+    });
+  });
+
+  client.on("msg", (data) => print(data));
+
+  client.onDisconnect((data) => print('Desconectado'));
+}
+
+Stream<String> readLine() => stdin.transform(utf8.decoder).transform(const LineSplitter());
